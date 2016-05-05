22973f02a1330665000c8ef3195b4045cdcfbdb7
liumy <liumy601@163.com>
2016-05-05 23:55:36 +0800

    fix testing issues of MEMORIALD-28

--- protected/views/layouts/main_normal_zoho.php
+++ protected/views/layouts/main_normal_zoho.php
@@ -41,6 +41,29 @@ text-decoration:none;
 <script type="text/javascript" src="/assets/jquery/signature/build/jquery.signaturepad.min.js<?php echo $GLOBALS['query_string']; ?>"></script>
 <script type="text/javascript" src="/assets/jquery/signature/build/json2.min.js<?php echo $GLOBALS['query_string']; ?>"></script>
 <link rel="stylesheet" href="/assets/jquery/signature/build/jquery.signaturepad.css<?php echo $GLOBALS['query_string']; ?>">
+
+<script>
+  window.intercomSettings = {
+    app_id: "k1tws933",
+    name: "<?php echo Yii::app()->user->firstname.' '.Yii::app()->user->lastname; ?>", // Full name
+    email: "<?php echo Yii::app()->user->email; ?>", // Email address
+    created_at: 1312182000 // Signup date as a Unix timestamp
+  };
+</script>
+<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/k1tws933';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
+
+<!-- App cues -->
+<link rel="stylesheet" type="text/css" href="//fast.appcues.com/appcues.min.css">
+<script src="//fast.appcues.com/17271.js"></script>
+<script>
+    Appcues.identify('1', {
+        email: '<?php echo Yii::app()->user->email; ?>',
+        name: '<?php echo Yii::app()->user->firstname.' '.Yii::app()->user->lastname; ?>',
+        // Additional user properties
+    });
+</script>
+<!-- App Cues -->
+
 <script type="text/javascript" src="<?php echo Yii::app()->params['appappSiteURL']; ?>/misc/appapp.js?v=1.5"></script>
 <?php
 if ($flashMsg = Yii::app()->user->getFlash('')) {
