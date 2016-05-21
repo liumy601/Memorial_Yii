a1987056e1282cd90f649e0169b6dd99575980c1
liumy <liumy601@163.com>
2016-05-21 15:36:16 +0800

    fix for MEMORIALD-46

--- protected/config/main.php
+++ protected/config/main.php
@@ -17,7 +17,10 @@ if(strpos($_SERVER['HTTP_HOST'], '127.0.0.1') !== false){
   $siteURL = 'http://app.memorialdirector.com';
 }
 
-
+$DBName = 'funeralb_db';
+$DBUser = 'root';
+$DBPass = '';
+$siteURL = 'http://dev.memorial.com/';
 
 // uncomment the following to define a path alias
 // Yii::setPathOfAlias('local','path/to/local-folder');
