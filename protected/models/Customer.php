c407f9f39ad2ab5bb32262fdcf34ba853b87f65d
liumy <liumy601@163.com>
2016-05-29 15:33:21 +0800

    fixes for MEMORIALD-37

--- protected/models/Customer.php
+++ protected/models/Customer.php
@@ -564,9 +564,8 @@ class Customer extends CActiveRecord
 
   public function save()
   {
-	  if( Yii::app()->user->hasState('company_id'))
+	if(empty($this->company_id) && Yii::app()->user->hasState('company_id'))
 		$this->company_id = Yii::app()->user->company_id;
-    
     return parent::save();
   }
 
--- protected/models/Task.php
+++ protected/models/Task.php
@@ -50,7 +50,7 @@ class Task extends CActiveRecord
   
   public function save()
   {
-	if( Yii::app()->user->hasState('company_id'))
+	if(empty($this->company_id) && Yii::app()->user->hasState('company_id'))
 		$this->company_id = Yii::app()->user->company_id;
     return parent::save();
   }
