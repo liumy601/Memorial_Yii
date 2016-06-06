964f628d006181e35587306752a9fae0472099ac
Support Preferati <support@preferati.com>
2016-06-06 19:27:33 -0400

    Bamboo production Nightly Commit - 06-Jun-2016

--- protected/views/layouts/main_normal_zoho.php
+++ protected/views/layouts/main_normal_zoho.php
@@ -52,7 +52,9 @@ $company_name = !empty($company) ? $company->name : '';
     name: "<?php echo Yii::app()->user->firstname.' '.Yii::app()->user->lastname; ?>", // Full name
     email: "<?php echo Yii::app()->user->email; ?>", // Email address
     created_at: 1312182000, // Signup date as a Unix timestamp
-	company: "<?php echo $company_name; ?>"
+	company: "<?php echo $company_name; ?>",
+    "site":"app.memorialdirector.com"
+
   };
 </script>
 <script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/k1tws933';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
