0118e7b39b887bd8f69b6ad482df46affa99979c
liumy <liumy601@163.com>
2016-06-07 23:12:18 +0800

    Fixes for MEMORIALD-37 and BB-85

--- protected/views/decedent/viewnewcustomer.php
+++ protected/views/decedent/viewnewcustomer.php
@@ -7,7 +7,7 @@
   <table cellpadding="5" cellspacing="0" width="100%" class="dvbtnlayer">
   <tbody>
   <tr>
-    <td align="center" nowrap="" class="pL15"><span appapptagid="16"></span> <a href="javascript:void(0);" onclick="history.back();return false;"><img src="/images/spacer.gif" class="backtoIcon" border="0"></a> </td>
+    <td align="center" nowrap="" class="pL15"> <a href="javascript:void(0);" onclick="ajaxNew('/decedent');setPageUrl('/decedent');return false;"><img src="/images/spacer.gif" class="backtoIcon" border="0"></a> </td>
     <td nowrap="" class="pL10 dvmo">
       <input class="dveditBtn dvcbtn buttonurl" type="button" value="Edit" name="Edit" url="/customer/updatenewcustomer/<?php echo $model->id; ?>" pagetitle="Edit contact" id="editview_student_<?php echo $model->id; ?>"/>
       <input name="Delete2" class="dvdelBtn dvcbtn" type="button" value="Delete" url="/customer/deletenewcustomer/<?php echo $model->id ?>" onclick="if(confirm('Are you sure delete this record?')){ ajaxRequest(this); }">
@@ -42,7 +42,6 @@
 <div style="float:right">
   <a href="javascript:void window.open('/customer/view/id/<?php echo $model->id ?>/print/true','printwin','menubar=1,status=0,resizable=1,scrollbars=1,toolbar=0,location=1')" class="utilsLink"><img src="/themes/Sugar/images/print.gif" width="13" height="13" alt="Print" border="0" align="absmiddle"></a>
   <a href="javascript:void window.open('/customer/view/id/<?php echo $model->id ?>/print/true','printwin','menubar=1,status=0,resizable=1,scrollbars=1,toolbar=0,location=1')" class="utilsLink">Print</a>
-  <span appapptagid="1"></span>
 </div>
 <?php
  }
@@ -447,7 +446,7 @@
 <td width="140" nowrap=""> 
 <a href="javascript:toggleRelatedList('relationship_product_<?php echo $model->id; ?>');" 
    name="relationship_product_<?php echo $model->id; ?>" id="relationship_product_<?php echo $model->id; ?>" class="relListHead">
-Products</a> <span appapptagid="17"></span>
+Products</a>
 </td>
 <td align="right"> <div id="ract_<?php echo $model->id; ?>" name="raction"> </td>
 </tr></tbody>
@@ -531,7 +530,7 @@ while ($row = $productDataProvider->read()) {
 <td width="140" nowrap=""> 
 <a href="javascript:toggleRelatedList('relationship_payment_<?php echo $model->id; ?>');" 
    name="relationship_payment_<?php echo $model->id; ?>" id="relationship_payment_<?php echo $model->id; ?>" class="relListHead">
-Payments</a> <span appapptagid="18"></span>
+Payments</a>
 </td>
 <td align="right"> <div id="ract_<?php echo $model->id; ?>" name="raction"> </td>
 </tr></tbody>
@@ -596,7 +595,7 @@ while ($row = $paymentDataProvider->read()) {
 <td width="140" nowrap=""> 
 <a href="javascript:toggleRelatedList('relationship_document_<?php echo $model->id; ?>');" 
    name="relationship_document_<?php echo $model->id; ?>" id="relationship_document_<?php echo $model->id; ?>" class="relListHead">
-Documents</a> <span appapptagid="19"></span>
+Documents</a>
 </td>
 <td align="right"> <div id="ract_<?php echo $model->id; ?>" name="raction"> </td>
 </tr></tbody>
@@ -660,7 +659,7 @@ if($documents){
 <td width="140" nowrap=""> 
 <a href="javascript:toggleRelatedList('relationship_contact_<?php echo $model->id; ?>');" 
    name="relationship_contact_<?php echo $model->id; ?>" id="relationship_contact_<?php echo $model->id; ?>" class="relListHead">
-Contacts</a> <span appapptagid="20"></span>
+Contacts</a>
 </td>
 <td align="right"> <div id="ract_<?php echo $model->id; ?>" name="raction"> </td>
 </tr></tbody>
