7a4d0e15fa27e39a1df65dcfafe83d6f2f44d98a
liumy <liumy601@163.com>
2016-06-18 12:00:13 +0800

    changes of MB-47

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,3 +1,10 @@
+<script>
+(function(i,s,o,g,r,a,m)
+Unknown macro: {i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) }
+)(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-46600175-2', 'auto');
+ga('send', 'pageview');
+</script>
 <?php $this->renderFile('protected/views/layouts/header.php'); ?>
 
 <?php $this->pageTitle=Yii::app()->name; ?>
