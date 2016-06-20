2fe6483323763bd145591302074de7dbecd9aff8
Support Preferati <support@preferati.com>
2016-06-20 19:27:54 -0400

    Bamboo production Nightly Commit - 20-Jun-2016

--- protected/views/layouts/main_normal_zoho.php
+++ protected/views/layouts/main_normal_zoho.php
@@ -60,6 +60,15 @@ $company_name = !empty($company) ? $company->name : '';
 <script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/k1tws933';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
 
 <!-- App cues -->
+<script type='text/javascript'>
+window.__wtw_lucky_site_id = 27428;
+
+	(function() {
+		var wa = document.createElement('script'); wa.type = 'text/javascript'; wa.async = true;
+		wa.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://cdn') + '.luckyorange.com/w.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(wa, s);
+	  })();
+	</script>
 <link rel="stylesheet" type="text/css" href="//fast.appcues.com/appcues.min.css">
 <script src="//fast.appcues.com/17271.js"></script>
 <script>
