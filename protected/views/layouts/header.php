23b9936076779a53208d105b01c417754a411af4
Support Preferati <support@preferati.com>
2016-06-23 19:27:25 -0400

    Bamboo production Nightly Commit - 23-Jun-2016

--- protected/views/layouts/header.php
+++ protected/views/layouts/header.php
@@ -3,6 +3,7 @@ if(Yii::app()->user->name != 'Guest' ){
 	$company = Company::model()->findByPk(Yii::app()->user->company_id);
 	$company_name = !empty($company) ? $company->name : '';
 ?>
+
 <script>
   window.intercomSettings = {
     app_id: "k1tws933",
@@ -28,10 +29,28 @@ if(Yii::app()->user->name != 'Guest' ){
 <!-- App Cues -->
 
 <?php
-}
+} else {
 ?>
 <!--white area, workflow-->
+<script>
+(function(i,s,o,g,r,a,m)
+Unknown macro: {i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) }
+)(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-46600175-2', 'auto');
+ga('send', 'pageview');
+</script>
+<script type='text/javascript'>
+window.__wtw_lucky_site_id = 27428;
 
+        (function() {
+                var wa = document.createElement('script'); wa.type = 'text/javascript'; wa.async = true;
+                wa.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://cdn') + '.luckyorange.com/w.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(wa, s);
+          })();
+        </script>
+<?php
+}
+?>
 <div class="bodycontainer"><div id="BodyContent">
 <table width="100%" cellspacing="0" cellpadding="0"><tbody><tr><td> 
 <div id="leftPanel">
